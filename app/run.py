4a0c66cb9f3ec553048c1239a8108a7f7cb08a2b
guolug <guolug@126.com>
2025-05-31 21:13:34 +0800

    完成所有功能的编写,项目version-1.0.0版本完成

--- app/run.py
+++ app/run.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from dotenv import load_dotenv
 import os
 from app.utils.config_utils_cookie import configure_utils_cookie, get_user_cookie
+import random
 
 # 加载环境变量配置
 load_dotenv()
@@ -17,7 +18,7 @@ def allIndex():
     """显示所有用户发布的问题列表"""
     if request.method == "GET":
         # 查询所有问题并关联用户信息
-        sql = "SELECT q.id id,q.title title,q.content content,q.views views,q.created_at created_at,u.username username " \
+        sql = "SELECT q.tag tag,q.id id,q.title title,q.content content,q.views views,q.created_at created_at,u.username username " \
               "FROM questions q JOIN users u ON q.user_id = u.id ORDER BY created_at DESC"
         with MysqlUtils() as db:
             all_data = db.fetch_all(sql, )
@@ -33,12 +34,10 @@ def index():
         if get_user_cookie(request) is None:
             # 未登录则重定向到登录页面
             return redirect(url_for('login'))
-
     # 查询当前用户发布的问题
-    sql = "SELECT id,title,content,views,created_at FROM questions WHERE user_id = %s ORDER BY created_at DESC"
+    sql = "SELECT id,title,content,views,created_at,tag FROM questions WHERE user_id = %s ORDER BY created_at DESC"
     with MysqlUtils() as db:
         user_issue = db.fetch_all(sql, (get_user_cookie(request)['id'],))
-
     # 渲染用户主页，传递用户信息和问题数据
     return render_template('index.html', user_data=get_user_cookie(request), user_issue=user_issue)
 
@@ -71,7 +70,9 @@ def login():
                                                   'id': users['id'],
                                                   'username': users['username'],
                                                   'score': users['score'],
-                                                  'email': users['email']
+                                                  'email': users['email'],
+                                                  'created_at': str(users['created_at']),
+                                                  'avatar': users['avatar']
                                               },
 
                                               max_age=3600)
@@ -85,10 +86,15 @@ def login():
 
 @app.route('/self', methods=['GET', 'POST'])
 def self():
+    sql = "SELECT id,title,content,views,created_at,tag FROM questions WHERE user_id = %s ORDER BY created_at DESC"
     """用户个人中心页面"""
     if request.method == 'GET':
         # 渲染个人中心页面，传递用户信息
-        return render_template('self.html', user_data=get_user_cookie(request))
+        with MysqlUtils() as db:
+            user_issue = db.fetch_all(sql, (get_user_cookie(request)['id'],))
+            counts = db.fetch_one("SELECT count(*) as counts FROM questions WHERE user_id = %s",
+                                  (get_user_cookie(request)['id'],))
+        return render_template('self.html', user_data=get_user_cookie(request), user_issue=user_issue, counts=counts)
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -103,6 +109,8 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    avatars = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png']
+    random_avatar = random.choice(avatars)
     """用户注册处理"""
     if request.method == 'GET':
         # 显示注册页面
@@ -123,10 +131,12 @@ def register():
 
                     # 用户名和邮箱均不存在，执行注册
                     print("不存在用户名和邮箱,可以注册")
-                    db.insert("INSERT INTO users(`username` ,`email`,`password_hash`) VALUES(%s,%s,%s) ",
+                    db.insert("INSERT INTO users(`username` ,`email`,`password_hash`,`avatar`) VALUES(%s,%s,%s,%s)",
                               (request.form.get('username'),
                                request.form.get('email'),
-                               request.form.get('password_hash')))
+                               request.form.get('password_hash'),
+                               random_avatar
+                               ))
                     flash('注册成功，请登录', 'success')  # 注册成功 添加成功消息
                 else:
                     flash('用户名或邮箱已存在', 'error')  # 注册失败 添加错误消息
@@ -138,12 +148,14 @@ def register():
 @app.route('/user/issue/<int:article_id>')
 def user_issue(article_id):
     """显示单个问题的详情页面"""
-    sql = "SELECT title,content,views,created_at FROM questions WHERE id = %s ORDER BY created_at DESC"
+    sql2 = "SELECT id FROM questions WHERE user_id = %s and id = %s"
+    sql = "SELECT id,title,content,views,created_at,tag FROM questions WHERE id = %s ORDER BY created_at DESC"
     with MysqlUtils() as db:
-        user_data = db.fetch_one(sql, ((article_id),))
-
+        issue_data = db.fetch_one(sql, ((article_id),))
+    with MysqlUtils() as db:
+        isMyIssue = db.fetch_one(sql2, (get_user_cookie(request)['id'], article_id))
     # 渲染问题详情页面，传递问题数据和用户信息
-    return render_template('issue.html', user_data=user_data, coociks=get_user_cookie(request))
+    return render_template('issue.html', issue_data=issue_data, user_data=get_user_cookie(request), isMyIssue=isMyIssue)
 
 
 @app.route('/add/issue/frompage', methods=['GET'])
@@ -162,6 +174,32 @@ def add_issue_submit():
             db.insert(sql, (title, content, get_user_cookie(request).get('id'), 0))
             return redirect(url_for('index'))
     return render_template('add_issue.html', user_data=get_user_cookie(request))
+
+
+@app.route('/profile/edit', methods=['GET', 'POST'])
+def profile_edit():
+    if request.method == 'POST':
+        sql = "UPDATE users SET username = %s,email=%s WHERE id = %s"
+        with MysqlUtils() as db:
+            db.execute(sql,
+                       (request.form.get('username'), request.form.get('email'), get_user_cookie(request).get('id')))
+        return redirect(url_for('logout'))
+    if request.method == 'GET':
+        return render_template('profile.html', user_data=get_user_cookie(request))
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    with MysqlUtils() as db:
+        question_data = db.fetch_one("SELECT * FROM questions WHERE id = %s", (id,))
+    if request.method == 'POST':
+        sql = "UPDATE questions SET title = %s,content=%s,tag=%s WHERE id = " + str(id)
+        with MysqlUtils() as db:
+            db.execute(sql,
+                       (request.form.get('title'), request.form.get('content'), request.form.get('tag')))
+        return redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('edit.html', user_data=get_user_cookie(request), question_data=question_data)
 
 
 if __name__ == '__main__':
